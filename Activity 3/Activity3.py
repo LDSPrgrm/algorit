91b7b3322a109feb87bf814fff90293001321f25
LDSPrgrm <ldsprgrm@gmail.com>
2024-02-06 11:57:51 +0800

    Modifed

--- Activity 3/Activity3.py
+++ Activity 3/Activity3.py
@@ -22,14 +22,20 @@ def load_csv(file_path):
 
 def search(keyword):
     keyword_upper = keyword.upper()
+    counter = 0
     for person in person_list:
         if(keyword_upper in person.first_name.upper() or
            keyword_upper in person.middle_name.upper() or
            keyword_upper in person.last_name.upper()):
+            counter += 1
             print(f"ID: {person.id}\nFirst Name: {person.first_name}\nMiddle Name: {person.middle_name}\nLast Name: {person.last_name}\nBirthday: {person.birthday}\nGender: {person.gender}\nAddress: {person.address}\n")
-
+    print(f"Successfully found {counter} results for \"{keyword}\" keyword.")
+    
+def main():
+    while True:
+        user_search = input("Search: ")
+        search(user_search)
+    
 person_list = load_csv("algorit/Activity 3/persons.csv")
-
-while True:
-    user_search = input("Search: ").upper()
-    search(user_search)
+if __name__ == "__main__":
+    main()
